73ab4f7b361a0fac63e1819da22688e1be873fee
Marcin <woolv2414@gmail.com>
2023-02-25 16:24:34 +0100

    Added searching and listening of ads.

--- records/ad.record.ts
+++ records/ad.record.ts
@@ -1,36 +1,41 @@
-import {AdEntity, NewAdEntity} from "../types";
+import {AdEntity, NewAdEntity, SimpleAdEntity} from "../types";
 import {ValidationError} from "../utils/error";
 import {pool} from "../utils/db";
 import {FieldPacket} from "mysql2";
 
-type AdRecordResults = [AdEntity[], FieldPacket[]]
+type AdRecordResults = [AdEntity[], FieldPacket[]];
 
 export class AdRecord implements AdEntity {
-    public description: string;
     public id: string;
     public name: string;
+    public description: string;
     public price: number;
     public url: string;
     public lat: number;
     public lon: number;
+
     constructor(obj: NewAdEntity) {
         if (!obj.name || obj.name.length > 100) {
-            throw new ValidationError('Nazwa ogłoszenie nie może być pusta ani przekraczać 100 znaków.');
-        }
-        if (obj.description.length > 1000) {
-            throw new ValidationError('Treść ogłoszenie  nie może przeekraczać pusta ani przekraczać 1000 .');
+            throw new ValidationError('Nazwa ogłoszenia nie może być pusta, ani przekraczać 100 znaków.');
         }
 
-        if(obj.price < 0 || obj.price > 999999) {
+        if (obj.description.length > 1000) {
+            throw new ValidationError('Treść ogłoszenia nie może przekraczać 1000 znaków.');
+        }
+
+        if (obj.price < 0 || obj.price > 999999) {
             throw new ValidationError('Cena nie może być mniejsza niż 0 lub większa niż 999999.');
         }
+
         // @TODO: Check if URL is valid!
         if (!obj.url || obj.url.length > 100) {
-            throw new ValidationError('Link ogłoszenia nie może być pusty ani przekraczać 100 znaków.');
+            throw new ValidationError('Link ogłoszenia nie może być pusty, ani przekraczać 100 znaków.');
         }
+
         if (typeof obj.lat !== 'number' || typeof obj.lon !== 'number') {
-            throw new ValidationError('Nie można zlokalizować ogłoszenia')
+            throw new ValidationError('Nie można zlokalizować ogłoszenia.');
         }
+
         this.id = obj.id;
         this.name = obj.name;
         this.description = obj.description;
@@ -41,9 +46,26 @@ export class AdRecord implements AdEntity {
     }
 
     static async getOne(id: string): Promise<AdRecord | null> {
-       const [results] = await pool.execute("SELECT * FROM `ads` WHERE id = :id",{ id,
-       }) as AdRecordResults;
+        const [results] = await pool.execute("SELECT * FROM `ads` WHERE `id` = :id", {
+            id,
+        }) as AdRecordResults;
 
-       return results.length === 0 ? null : new AdRecord(results[0]);
+        return results.length === 0 ? null : new AdRecord(results[0]);
+    }
+
+    static async findAll(name: string): Promise<SimpleAdEntity[]> {
+        const [results] = await pool.execute("SELECT * FROM `ads` WHERE `name` LIKE :search", {
+            search: `%${name}%`,
+        }) as AdRecordResults;
+
+        return results.map(result => {
+            const {
+                id, lat, lon,
+            } = result;
+
+            return {
+                id, lat, lon,
+            };
+        });
     }
 }
